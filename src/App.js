45b83a9c199771dcf0e327386e46931c69d8b12f
Levi Liester <levi.liester@gmail.com>
2022-01-25 21:29:18 -0600

    Alrighty.  This seems to be able to do the basic shapes correctly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useState, useEffect, useRef} from 'react'
+import {getNextGeneration} from "./game-logic";
 
 //Shamelessly stolen
 function useInterval(callback, delay) {
@@ -15,21 +16,24 @@ function useInterval(callback, delay) {
         function tick() {
             savedCallback.current();
         }
+
         if (delay !== null) {
             let id = setInterval(tick, delay);
             return () => clearInterval(id);
         }
     }, [delay]);
 }
+
 const initialGameState = {
     "id": "abc123",
     "generationCount": 5,
     "size": 5,
-    "world": [[1,0,0,0,1],
-        [1,0,0,1,1],
-        [1,0,1,0,0],
-        [1,1,0,1,1],
-        [1,0,1,0,0]]
+    "world": [
+        [0, 1, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]]
 }
 
 function GameBoard({gameState}) {
@@ -50,22 +54,27 @@ function GameBoard({gameState}) {
 
 function App() {
     const [delay, setDelay] = useState(1000);
+    const [gameState, setGameState] = useState(initialGameState)
 
     useInterval(() => {
         console.log('evaluating next state')
-        const newGameState = {...initialGameState}
-        setGameState(newGameState)
+        const newGameState = getNextGeneration(gameState)
+        setGameState({
+            ...gameState,
+            world: newGameState
+        })
     }, delay);
 
     function handleDelayChange(e) {
         setDelay(Number(e.target.value));
     }
-  const [gameState, setGameState] = useState(initialGameState)
-  return (
-    <div className="App">
-      <GameBoard gameState={gameState}/>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <GameBoard gameState={gameState}/>
+            Speed: <input value={delay} onChange={event => setDelay(event.target.value)}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/game-logic.js
@@ -0,0 +1,106 @@
+const shouldLog = true;
+
+const log = (string) => {
+    if(shouldLog){
+        console.log(string)
+    }
+}
+
+const hasNeighbor = function hasNeighbor(world, row, column){
+    return world[row] ? [1,2].includes(world[row][column]): false
+}
+
+const getNeighbors = function getNeighbors(world, row, column){
+    //check left
+    let left = hasNeighbor(world, row, column-1)
+    //check top left
+    let topLeft = hasNeighbor(world, row-1, column-1)
+    //check top
+    let top = hasNeighbor(world,row-1,column)
+    //check top right
+    let topRight = hasNeighbor(world, row-1, column+1)
+    //check right
+    let right = hasNeighbor(world, row, column+1)
+    //check bottom right
+    let bottomRight = hasNeighbor(world, row+1, column+1)
+    //check bottom
+    let bottom = hasNeighbor(world,row+1,column)
+    //check bottom left
+    let bottomLeft = hasNeighbor(world,row+1,column-1)
+
+    log('has left:', !!left)
+    log('has topLeft:', !!topLeft)
+    log('has top:', !!top)
+    log('has topRight:', !!topRight)
+    log('has right:', !!right)
+    log('has bottomRight:', !!bottomRight)
+    log('has bottom:', !!bottom)
+    log('has bottomLeft:', !!bottomLeft)
+    const totalNeighbors = left + topLeft + top + topRight + right + bottomRight + bottom + bottomLeft;
+    console.log(`[${row},${column}]: ${totalNeighbors}`)
+    return totalNeighbors;
+}
+
+const printGame = function printGame(world){
+    console.table(world);
+}
+
+const updateWorld = function updateWorld(world){
+    for(let i = 0; i<5;i++){
+        for(let j =0; j< 5;j++){
+            if(world[i][j] === 3){
+                world[i][j] = 1;
+            }
+            if(world[i][j] === 2){
+                world[i][j] = 0;
+            }
+        }
+    }
+}
+
+// 0: dead cell
+// 1: alive cell
+// 2: will die
+// 3: will live/create
+
+// let world =
+//     [
+//         [0,0,0,0,0],
+//         [1,1,1,0,0],
+//         [0,0,0,0,0],
+//         [0,0,0,0,0],
+//         [0,0,0,0,0]
+//     ]
+
+const getNextGeneration = function getNextGeneration({world}){
+    for(let i = 0; i<5;i++){
+        for(let j =0; j< 5;j++){
+            log('checking:', i, ',', j)
+            const neighbors =  getNeighbors(world, i,j)
+            const cellAlive = world[i][j];
+            if(cellAlive){
+                if(neighbors < 2 || neighbors > 3){
+                    log(`[${i},${j}]:alive, has ${neighbors} and will die`)
+                    world[i][j] = 2
+                }else{
+                    log(`[${i},${j}]:alive, has ${neighbors} and will survive`)
+                }
+            }else {
+                if(neighbors === 3) {
+                    log(`[${i},${j}]:dead, has ${neighbors} and will create`)
+                    world[i][j] = 3; //create cell
+                }else {
+                    log(`[${i},${j}]:dead, has ${neighbors} and will stay dead`)
+                }
+            }
+            // printGame(world)
+        }
+    }
+    printGame(world)
+    updateWorld(world)
+    return world;
+}
+
+export {
+    getNextGeneration
+}
